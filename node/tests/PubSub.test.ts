28da9f1f9da120d57ceb2298fea90781c4e40c80
hank95179 <hank95179@gmail.com>
2025-10-16 07:52:41 -0500

    add delay

--- node/tests/PubSub.test.ts
+++ node/tests/PubSub.test.ts
@@ -1750,6 +1750,7 @@ describe("PubSub", () => {
                     getOptions(clusterMode),
                     pubSubPattern,
                 );
+                await new Promise((resolve) => setTimeout(resolve, 1000));
 
                 // Publish messages to all channels
                 for (const [channel, message] of allChannelsAndMessages) {
